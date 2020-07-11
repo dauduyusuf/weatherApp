1a0d7b39002a9e1387a6d54fee6970f832da4389
VictorDaudu <Daudu.victor01@gmail.com>
2020-07-11 04:48:47 +0100

    Updates

---
+++ precache-manifest.0227df9e944cc9b321541f2bd1aa6b9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c21f3e98e5d21419eeff186a54f8e864",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "6455d4dd17bc527de110",
+    "url": "/weatherApp/static/css/main.065f1018.chunk.css"
+  },
+  {
+    "revision": "c7aba720ccc7adbaae2e",
+    "url": "/weatherApp/static/js/2.8d9f920a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherApp/static/js/2.8d9f920a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6455d4dd17bc527de110",
+    "url": "/weatherApp/static/js/main.4d12c441.chunk.js"
+  },
+  {
+    "revision": "efba474b3fd5a4a7aa5f",
+    "url": "/weatherApp/static/js/runtime-main.d4919a9c.js"
+  },
+  {
+    "revision": "0077d21d5cc2db656d8c1a170b2bbf76",
+    "url": "/weatherApp/static/media/bg1.0077d21d.jpg"
+  }
+]);
